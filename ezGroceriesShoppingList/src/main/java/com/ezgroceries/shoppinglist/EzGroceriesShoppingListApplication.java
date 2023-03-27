026d980c1e56a20befa68662c2088d4686835567
minokabaivanov <kabaivanov.mino@gmail.com>
2023-03-27 14:41:16 +0300

    lab-01-springboot

--- ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
@@ -1,10 +1,13 @@
 package com.ezgroceries.shoppinglist;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EzGroceriesShoppingListApplication {
+
     public static void main(String[] args) {
-        SpringBootApplication.run(EzGroceriesShoppingListApplication.class,args);
+        SpringApplication.run(EzGroceriesShoppingListApplication.class, args);
     }
+
 }
