b852ef42b92a98d6d0a08cab1ab5654f7ebebe06
Mino Kabaivanov <jf24452@kbc-group.com>
2023-03-27 11:35:13 +0300

    Innitial commit

---
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
@@ -0,0 +1,10 @@
+package com.ezgroceries.shoppinglist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzGroceriesShoppingListApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(EzGroceriesShoppingListApplication.class,args);
+    }
+}
